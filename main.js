e8fa8aba117353379835a8c5c0e7e8c75bbccfc9
hyunjoo.kim <khjoo1203@gmail.com>
2025-06-20 16:53:37 +0900

    카테고리별 검색

---
+++ main.js
@@ -0,0 +1,57 @@
+const API_KEY = `acf8fc54b6204f2ab344c80407b0e3ce`;
+let newsList = [];
+const menus = document.querySelectorAll('.menus button');
+menus.forEach((menu) =>
+  menu.addEventListener('click', (event) => {
+    getNewsByCategory(event);
+  })
+);
+const getNewsByCategory = async (event) => {
+  const category = event.target.textContent.toLowerCase();
+  console.log('category', category);
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log('data', data);
+  newsList = data.articles;
+
+  render();
+};
+const getLatestNews = async () => {
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+  //   console.log('data', newsList);
+};
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news">
+          <div class="col-lg-4">
+            <img class="news-img-size" src="${news.urlToImage}" alt="" />
+          </div>
+          <div class="col-lg-8">
+            <h2>${news.title}</h2>
+            <p>${news.description}</p>
+            <div>
+              ${news.source.name} * ${news.publishedAt}
+            </div>
+          </div>
+        </div>`
+    )
+    .join('');
+  //   console.log('html', newsHTML);
+
+  document.getElementById('news-board').innerHTML = newsHTML;
+};
+getLatestNews();
+
+//1. 버튼들에 클릭 이벤트 주기
+//2. 카테고리별 뉴스 가져오기
+//3. 그 뉴스를 보여주기
