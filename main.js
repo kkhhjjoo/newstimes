0f574e682b848b2aaf195aeb06df2b0e1655c037
hyunjoo.kim <khjoo1203@gmail.com>
2025-06-21 13:12:37 +0900

    키워드별 검색

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const API_KEY = `acf8fc54b6204f2ab344c80407b0e3ce`;
 let newsList = [];
 const menus = document.querySelectorAll('.menus button');
+//console.log('menus', menus);
 menus.forEach((menu) =>
-  menu.addEventListener('click', (event) => {
-    getNewsByCategory(event);
-  })
+  menu.addEventListener('click', (event) => getNewByCategory(event))
 );
-const getNewsByCategory = async (event) => {
+
+const getLatestNews = async () => {
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  newsList = data.articles;
+  render();
+  console.log('data', newsList);
+};
+const getNewByCategory = async (event) => {
   const category = event.target.textContent.toLowerCase();
   console.log('category', category);
   const url = new URL(
@@ -16,19 +26,22 @@ const getNewsByCategory = async (event) => {
   const data = await response.json();
   console.log('data', data);
   newsList = data.articles;
-
   render();
 };
-const getLatestNews = async () => {
+
+const getNewsByKeyword = async () => {
+  const keyword = document.getElementById('search-input').value;
+  console.log('keyword', keyword);
   const url = new URL(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+    `https://newsapi.org/v2/top-headlines?country=us&q=${keyword}&apiKey=${API_KEY}`
   );
   const response = await fetch(url);
   const data = await response.json();
+  console.log('keyword data', data);
   newsList = data.articles;
   render();
-  //   console.log('data', newsList);
 };
+
 const render = () => {
   const newsHTML = newsList
     .map(
@@ -46,12 +59,9 @@ const render = () => {
         </div>`
     )
     .join('');
-  //   console.log('html', newsHTML);
-
   document.getElementById('news-board').innerHTML = newsHTML;
 };
 getLatestNews();
-
 //1. 버튼들에 클릭 이벤트 주기
 //2. 카테고리별 뉴스 가져오기
 //3. 그 뉴스를 보여주기
